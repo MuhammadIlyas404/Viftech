6aafcd5ac9e9121c2b34d88f9b82ecd3fa6f5272
muhammad ilyas <milyas@creamsolutions.com>
2023-08-07 23:45:22 +0500

    Add project files.

---
+++ Viftech/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using Viftech.Models;
+
+namespace Viftech.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult PostRequest(PostModel postModel)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=ViftechDB;Integrated Security=True");
+            con.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM  AgeLoad WHERE Age = "+ postModel.Age + "", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+
+            var response = new ResponseModel();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                var day = (postModel.EndDate - postModel.StartDate).TotalDays;
+                response.Total = (Convert.ToDecimal(day) * 3) * (Convert.ToDecimal(row["LoadAge"])* Convert.ToDecimal(day));
+                response.Currency = postModel.Currency;
+                response.QuotationId = Convert.ToInt32(row["AgeLoadId"]);
+            }
+            con.Close();
+            ViewBag.Data = response; 
+
+            return View("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Viftech/Models/PostModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Viftech.Models
+{
+    public class PostModel
+    {
+        public decimal Age { get; set; }
+        public string? Currency { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public class ResponseModel
+    {
+        public decimal Total { get; set; }
+        public string? Currency { get; set; }
+        public int QuotationId { get; set; }
+    }
+}
